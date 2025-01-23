930072c76d4e92ab69be6dd14e0a95ed017acfbd
JuCmd <yhalubkova@gmail.com>
2025-01-23 16:58:01 +0200

    addedfirst file

---
+++ loan.py
@@ -0,0 +1,10 @@
+#Dictionary - key ans values pairs
+
+acronyms = {'LOL': 'laugh out loud',
+            'IDK': "I don't know",
+            'TBH': 'to be honest'}
+
+print(acronyms['LOL']) # to look up a value in a dictinary
+
+acronyms['HAH'] = 'Hello'
+print(acronyms)
